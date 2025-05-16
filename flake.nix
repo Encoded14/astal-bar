8d63e512e9d331888f1d05f980f3a66dcd82ed11
Linus Kentzler <linusken@tuta.io>
2025-05-16 23:05:29 +0200

    added dependency pkgs

--- flake.nix
+++ flake.nix
@@ -1,25 +1,44 @@
 {
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    astal = {
-      url = "github:aylur/astal";
-      inputs.nixpkgs.follows = "nixpkgs";
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+        astal = {
+        url = "github:aylur/astal";
+        inputs.nixpkgs.follows = "nixpkgs";
     };
-  };
+};
 
-  outputs = { self, nixpkgs, astal }: let
+outputs = { self, nixpkgs, astal }: let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
-  in {
-    packages.${system}.default = astal.lib.mkLuaPackage {
-      inherit pkgs;
-      name = "astal";
-      src = ./.; 
+    in {
+        packages.${system}.default = astal.lib.mkLuaPackage {
+            inherit pkgs;
+            name = "astal";
+            src = ./.; 
 
-      extraPackages = [
-        astal.packages.${system}.battery
-        pkgs.dart-sass
-      ];
+            extraPackages = with astal.packages.${system};
+                apps
+                astal3
+                battery
+                bluetooth
+                io
+                mpris
+                network
+                notifd
+                powerprofiles
+                tray
+                wireplumber
+            ]
+            ++ (with pkgs; [
+                dart-sass
+                inotify-tools
+                brightnessctl
+                gammastep
+            ])
+            ++ (with pkgs.lua52Packages; [
+                cjson
+                luautf8
+            ]);
+        };
     };
-  };
 }
--- init.lua
+++ init.lua
@@ -4,6 +4,5 @@ local Bar = require("widget.Bar")
 App:start {
     main = function()
         Bar(0)
-        Bar(1) -- instantiate for each monitor
     end,
 }
